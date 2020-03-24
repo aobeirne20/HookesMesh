8083233ce395e32b806a67a8464ca53d93405f0a
Tim Krasnoperov <tkrasnoperov@gmail.com>
2020-03-24 00:23:32 -0700

    tkrasnoperov - added mesh visualizer

---
+++ MeshVisual.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from graphics_utils import *
+from SpaceCanvasMap import SpaceCanvasMap
+
+class MeshVisual:
+    def __init__(   self,
+                    mesh,
+                    space_left=-1.5,
+                    space_right=4.5,
+                    space_top=-4,
+                    space_bottom=4,
+                    visual_width=400,
+                    visual_height=400,
+                    point_radius=4,
+                    fill="#FFF"):
+
+        self.mesh = mesh
+        self.space_canvas_map = SpaceCanvasMap(
+            space_left,
+            space_right,
+            space_top,
+            space_bottom,
+            visual_width,
+            visual_height
+        )
+        self.gui, self.canvas = setup_graphics(width=visual_width, height=visual_height)
+        self.point_radius = point_radius
+        self.fill = fill
+
+        self.handles = []
+        for rigid in np.nditer(self.mesh.m, flags=["refs_ok"]):
+            rigid = rigid.item()
+            canvas_x, canvas_y = self.space_canvas_map.space_to_canvas(rigid.pos)
+
+            handle = self.canvas.create_oval(
+                canvas_x - self.point_radius,
+                canvas_y - self.point_radius,
+                canvas_x + self.point_radius,
+                canvas_y + self.point_radius,
+                fill=self.fill
+            )
+            self.handles.append(handle)
+
+    def update(self):
+        for rigid, handle in zip(np.nditer(self.mesh.m, flags=["refs_ok"]), self.handles):
+            rigid = rigid.item()
+            canvas_x, canvas_y = self.space_canvas_map.space_to_canvas(rigid.pos)
+            self.canvas.coords(
+                handle,
+                canvas_x - self.point_radius,
+                canvas_y - self.point_radius,
+                canvas_x + self.point_radius,
+                canvas_y + self.point_radius
+            )
+            self.canvas.itemconfig(handle, fill=self.fill)
+
+        self.gui.update()
---
+++ SpaceCanvasMap.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class SpaceCanvasMap():
+    def __init__(self, space_left, space_right, space_top, space_bottom, canvas_width, canvas_height):
+        self.canvas_width = canvas_width
+        self.canvas_height = canvas_height
+        self.space_width = space_right - space_left
+        self.space_height = space_bottom - space_top
+
+        self.space_left = space_left
+        self.space_right = space_right
+        self.space_top = space_top
+        self.space_bottom = space_bottom
+        self.center = np.array([
+            np.mean([space_left, space_right]),
+            np.mean([space_top, space_bottom])
+        ])
+
+        self.space_translate = np.array([space_left, space_top])
+        self.space_scale = np.array([
+            self.space_width / self.canvas_width,
+            self.space_height / self.canvas_height
+        ])
+
+        self.canvas_translate = -self.space_translate
+        self.canvas_scale = 1 / self.space_scale
+
+    def space_to_canvas(self, point):
+        return self.canvas_scale * (self.canvas_translate + np.flip(point[1:], axis=0))
+
+    def canvas_to_space(self, point):
+        return np.append(self.space_translate + (self.space_scale * point), [0])
