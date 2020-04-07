179d0e20b63b3cc87d34868ab6a6e2e6f7c70620
aobeirne20 <56138845+aobeirne20@users.noreply.github.com>
2020-04-06 22:03:09 -0700

    added magnitude functionality

--- Main.py
+++ Main.py
@@ -88,6 +88,7 @@ class Instance:
         self.tracked_objects = None
         self.tracked_axis = None
         self.motion_tracker = None
+        self.mag_tracker = None
         self.energy_tracker = None
         self.force_d = dict()
         self.visual = MeshVisual(mesh, 1)
@@ -104,10 +105,14 @@ class Instance:
         self.energy_tracker[0, 2] = self.energy_tracker[0, 0] + self.energy_tracker[0, 1]
 
         self.motion_tracker = np.zeros([int(self.extent / self.t), len(tracked_object_locs)])
+        self.mag_tracker = np.zeros([int(self.extent / self.t), len(tracked_object_locs)])
         self.tracked_objects = [self.mesh.m[tuple(loc)] for loc in tracked_object_locs]
         self.tracked_axis = tracked_axis
         for i, axis in enumerate(self.tracked_axis):
             self.motion_tracker[0, i] = self.tracked_objects[i].pos[axis-1]
+            self.mag_tracker[0, i] = np.linalg.norm(self.tracked_objects[i].pos - self.tracked_objects[i].initial_pos)
+
+
 
     def initialize_displacement(self, starting_objects_locs, displacements):
         for loc, disp in zip(starting_objects_locs, displacements):
@@ -149,10 +154,12 @@ class Instance:
                     rigid.forces = np.zeros(3, dtype=float)
 
             for j, (rigid, axis) in enumerate(zip(self.tracked_objects, self.tracked_axis)):
-                self.motion_tracker[i+1, j] = self.tracked_objects[j].pos[axis - 1]
+                self.motion_tracker[i+1, j] = self.tracked_objects[j].pos[axis-1]
+                self.mag_tracker[i+1, j] = np.linalg.norm(self.tracked_objects[j].pos - self.tracked_objects[j].initial_pos)
+
 
             self.energy_tracker[i + 1, 2] = self.energy_tracker[i + 1, 0] + self.energy_tracker[i + 1, 1]
-            self.visual.update()
+            #self.visual.update()
 
     def graph_motion(self):
         self.sin_set = np.sin(self.time_axis)
@@ -160,33 +167,32 @@ class Instance:
         ax = motion_plot.add_subplot()
         for j, rigid in enumerate(self.tracked_objects):
             ax.plot(self.time_axis, self.motion_tracker[:, j], label=f"Rigid at {rigid.initial_pos}")
+            ax.plot(self.time_axis, self.mag_tracker[:, j], label=f"Mag ofRigid at {rigid.initial_pos}")
         ax.set_xlabel("Time (s)")
         ax.set_ylabel("Position (m)")
         ax.set_title("Displacements in a Hookean System")
         ax.legend()
         motion_plot.show()
-        m1 = self.motion_tracker[0::30, 0]
-        m2 = self.motion_tracker[0::30, 1]
-        m3 = self.motion_tracker[0::30, 2]
-        m4 = self.motion_tracker[0::30, 3]
-        m5 = self.motion_tracker[0::30, 4]
-        np.savetxt('3DOFMotion.csv', np.asarray(list(zip(self.time_axis[0::30], m1, m2, m3, m4, m5))), delimiter=',', fmt='%1.8f')
+        m1 = self.motion_tracker[0::40, 0]
+        m2 = self.motion_tracker[0::40, 1]
+        m3 = self.mag_tracker[0::40, 0]
+        np.savetxt('3DOFMotion.csv', np.asarray(list(zip(self.time_axis[0::30], m1, m2, m3))), delimiter=',', fmt='%1.8f')
 
 
 
     def graph_energy(self):
-        #energy_plot = plt.figure()
-        #ax = energy_plot.add_subplot()
-        #ax.plot(self.time_axis, self.energy_tracker[:, 0], label="Kinetic Energy")
-        #ax.plot(self.time_axis, self.energy_tracker[:, 1], label="Potential Energy")
-        #ax.plot(self.time_axis, self.energy_tracker[:, 2], label="Total Energy")
-        #ax.set_xlabel("Time (s)")
-        #ax.set_ylabel("Energy (kJ)")
-        #ax.set_title("Energy of a Hookean System")
-        #ax.legend()
-        #energy_plot.show()
-        t = self.energy_tracker[0::10000, 2]
-        np.savetxt('2DOFEnergy.csv', np.asarray(list(zip(self.time_axis[0::10000], t))), delimiter=',', fmt='%1.8f')
+        energy_plot = plt.figure()
+        ax = energy_plot.add_subplot()
+        ax.plot(self.time_axis, self.energy_tracker[:, 0], label="Kinetic Energy")
+        ax.plot(self.time_axis, self.energy_tracker[:, 1], label="Potential Energy")
+        ax.plot(self.time_axis, self.energy_tracker[:, 2], label="Total Energy")
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("Energy (kJ)")
+        ax.set_title("Energy of a Hookean System")
+        ax.legend()
+        energy_plot.show()
+        #t = self.energy_tracker[0::10000, 2]
+        #np.savetxt('2DOFEnergy.csv', np.asarray(list(zip(self.time_axis[0::10000], t))), delimiter=',', fmt='%1.8f')
 
     def simple_fourier(self):
         frequency_plot = plt.figure()
@@ -195,15 +201,21 @@ class Instance:
         for j, rigid in enumerate(self.tracked_objects):
             fourier = np.fft.fft(self.motion_tracker[:, j] - np.mean(self.motion_tracker[:, j]))
             fourier = np.fft.fftshift(fourier)
+            fourier_mag = np.fft.fft(self.mag_tracker[:, j])
+            fourier_mag = np.fft.fftshift(fourier_mag)
             freq = np.fft.fftfreq(self.time_axis.shape[-1], d=self.t)
             freq = np.fft.fftshift(freq)
             ax.plot(freq, np.absolute(fourier))
+            ax.plot(freq, np.absolute(fourier_mag))
             fourier = np.abs(fourier)
+            fourier_mag = np.abs(fourier_mag)
             freq = freq[int(0.5*self.extent/self.t):int((1/self.t)+(0.5*self.extent/self.t))]
             f = fourier[int(0.5*self.extent/self.t):int((1/self.t)+(0.5*self.extent/self.t))]
+            fm = fourier_mag[int(0.5*self.extent/self.t):int((1/self.t)+(0.5*self.extent/self.t))]
             np.savetxt('3DOFFourier.csv', freq, delimiter=',', fmt='%1.8f')
             np.savetxt(f'Fourier{j}.csv', f, delimiter=',', fmt='%1.6f')
-            ax.set_xlim((0, 1))
+            np.savetxt(f'Fouriermag{j}.csv', fm, delimiter=',', fmt='%1.6f')
+            ax.set_xlim((0, 0.5))
         frequency_plot.show()
 
     def deviation_from_ideal(self, analytical):
@@ -235,19 +247,19 @@ for y in range(0, 5):
         if y == 4 and x == 4:
             pass
         elif y == 4 and x != 4:
-            TrialMesh.create_rest_spring([0, x, y], [0, x + 1, y], 1)
+            TrialMesh.create_rest_spring([0, x, y], [0, x + 1, y], 3)
         elif x == 4 and y != 4:
-            TrialMesh.create_rest_spring([0, x, y], [0, x, y + 1], 1)
+            TrialMesh.create_rest_spring([0, x, y], [0, x, y + 1], 3)
         else:
-            TrialMesh.create_rest_spring([0, x, y], [0, x, y + 1], 1)
-            TrialMesh.create_rest_spring([0, x, y], [0, x + 1, y], 1)
+            TrialMesh.create_rest_spring([0, x, y], [0, x, y + 1], 3)
+            TrialMesh.create_rest_spring([0, x, y], [0, x + 1, y], 3)
 
 
 
-Instance1 = Instance(TrialMesh, 0.01, 60)
-Instance1.initialize_displacement([[0, 2, 0]], [[0, 0, 0.3]])
+Instance1 = Instance(TrialMesh, 0.001, 200)
+Instance1.initialize_displacement([[0, 2, 0]], [[0, 0, 0.05]])
 #Instance1.initialize_displacement([[0, 1, 0], [0, 2, 0], [0, 3, 0], [0, 4, 0], [0, 0, 0]], [[0, 0, 0.3], [0, 0, 0.3], [0, 0, 0.3], [0, 0, 0.3], [0, 0, 0.3]])
-Instance1.initialize_tracking([[0, 2, 0], [0, 2, 1], [0, 2, 2], [0, 2, 3], [0, 2, 4]], [3, 3, 3, 3, 3])
+Instance1.initialize_tracking([[0, 1, 1], [0, 1, 1]], [2, 3])
 Instance1.simulate()
 Instance1.graph_motion()
 #Instance1.graph_energy()
